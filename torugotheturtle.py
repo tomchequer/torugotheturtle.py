d7f77efcb07f0dc7e9b58b8e436ef8d729c87593
antonio <antoniochequer@gmail.com>
2021-08-09 15:17:08 -0300

    Now you can hit play and see a spirograph. Enjoy changing the settings and playing a bit.

--- torugotheturtle.py
+++ torugotheturtle.py
@@ -1,15 +1,37 @@
-import turtle
+import turtle as t
+import random
 
-
-torugo = turtle.Turtle()
+torugo = t.Turtle()
 
 torugo.shape('turtle')
 
-torugo.color('indigo')
+t.colormode(255)
+
+def actualgenrandcolor():
+    return (random.randint(0, 255),random.randint(0, 255),random.randint(0, 255))
+
+#def randomwalk(walks):
+    for i in range(walks):
+        torugo.speed(random.randint(1,6))
+        torugo.pensize(random.randint(5, 15))
+        torugo.setheading(randomangle())
+        #torugo.right(random.randint(0,360))
+        torugo.forward(random.randint(1, 50))
+        torugo.color(actualgenrandcolor())
+
+torugo.pensize(2)
+torugo.speed('fastest')
 
 
+def drawcircle(size):
+    for i in range (int(360 / size)):
+        torugo.color(actualgenrandcolor())
+        torugo.circle(100)
+        torugo.setheading(torugo.heading() + size)
 
 
+drawcircle(4)
 
-display = turtle.Screen()
+display = t.Screen()
 display.exitonclick()
+
